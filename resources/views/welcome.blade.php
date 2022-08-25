6738a1dc8b6701bb8eaf60666638ba45c6be4afc
Victor Kauan <victorkauan.dev@gmail.com>
2022-08-25 13:59:03 -0300

    feat: display formatted CPF and phone number on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,13 +21,34 @@
             </thead>
             <tbody>
                 @foreach ($users as $user)
+                    @php
+                        $cpf_one = substr($user->cpf, 0, 3);
+                        $cpf_two = substr($user->cpf, 3, 3);
+                        $cpf_three = substr($user->cpf, 6, 3);
+                        $cpf_four = substr($user->cpf, 9, 2);
+
+                        $formatted_cpf = "$cpf_one.$cpf_two.$cpf_three-$cpf_four";
+
+                        $phone_one = substr($user->phone, 0, 2);
+                        if (strlen((string) $user->phone) === 10) {
+                            $phone_two = substr($user->phone, 2, 4);
+                            $phone_three = substr($user->phone, 6, 4);
+                        }
+                        else {
+                            $phone_two = substr($user->phone, 2, 5);
+                            $phone_three = substr($user->phone, 7, 4);
+                        }
+
+                        $formatted_phone = "($phone_one) $phone_two-$phone_three";
+                    @endphp
+
                     <tr>
                         <th scope="row">{{ $user->id }}</th>
                         <td>{{ $user->name }}</td>
-                        <td>{{ $user->cpf }}</td>
+                        <td>{{ $formatted_cpf }}</td>
                         <td>{{ $user->email }}</td>
                         <td>{{ $user->address }}</td>
-                        <td>{{ $user->phone }}</td>
+                        <td>{{ $formatted_phone }}</td>
                         <td>{{ date('d/m/Y', strtotime($user->created_at)) }}</td>
                         <td>
                             <a href="#">Editar</a>
