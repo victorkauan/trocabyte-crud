ce16f4c1130f1295e30e191c804a7202d6d55c5b
Victor Kauan <victorkauan.dev@gmail.com>
2022-08-25 21:00:25 -0300

    feat and fix: create custom README and fix users table columns and footer styling

--- database/migrations/2022_08_25_140729_create_users_table.php
+++ database/migrations/2022_08_25_140729_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->bigInteger('cpf');
+            $table->bigInteger('cpf')->unique();
             $table->string('email')->unique();
             $table->text('address');
             $table->bigInteger('phone');
